0e6e41cc5782248a14d0d30dfad4d05fcdee53aa
Tomaae <23486452+tomaae@users.noreply.github.com>
2022-03-13 00:37:01 +0100

    added more attribute formatting values

--- custom_components/truenas/helper.py
+++ custom_components/truenas/helper.py
@@ -7,7 +7,11 @@
 def format_attribute(attr):
     res = attr.replace("_", " ")
     res = res.capitalize()
+    res = res.replace("zfs", "ZFS")
     res = res.replace(" gib", " GiB")
     res = res.replace("Cpu ", "CPU ")
+    res = res.replace("Vcpu ", "vCPU ")
     res = res.replace("Vmware ", "VMware ")
+    res = res.replace("Ip4 ", "IP4 ")
+    res = res.replace("Ip6 ", "IP6 ")
     return res
