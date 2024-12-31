ccd415a6eaa7a3c26768a68d344b37a96eac151b
sammiq <968135+sammiq@users.noreply.github.com>
2024-12-31 13:00:21 +1100

    Don't go offline unless request throws exception

--- custom_components/truenas/api.py
+++ custom_components/truenas/api.py
@@ -67,76 +67,67 @@ class TrueNASAPI(object):
         self, service: str, method: str = "get", params: dict[str, Any] | None = {}
     ) -> Optional(list):
         """Retrieve data from TrueNAS."""
-        self.lock.acquire()
-        error = False
-        try:
-            _LOGGER.debug(
-                "TrueNAS %s query: %s, %s, %s",
-                self._host,
-                service,
-                method,
-                params,
-            )
-
-            headers = {
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {self._api_key}",
-            }
-            if method == "get":
-                response = requests_get(
-                    f"{self._url}{service}",
-                    headers=headers,
-                    params=params,
-                    verify=self._ssl_verify,
-                    timeout=10,
-                )
-
-            elif method == "post":
-                response = requests_post(
-                    f"{self._url}{service}",
-                    headers=headers,
-                    json=params,
-                    verify=self._ssl_verify,
-                    timeout=10,
-                )
-
-            if response.status_code == 200:
-                data = response.json()
-                _LOGGER.debug("TrueNAS %s query response: %s", self._host, data)
-            else:
-                error = True
-        except Exception:
-            error = True
-
-        if error:
+        error = None
+        with self.lock:
             try:
-                errorcode = response.status_code
-            except Exception:
-                errorcode = "no_response"
+                _LOGGER.debug(
+                    "TrueNAS %s query: %s, %s, %s",
+                    self._host,
+                    service,
+                    method,
+                    params,
+                )
 
-            _LOGGER.warning(
-                'TrueNAS %s unable to fetch data "%s" (%s)',
-                self._host,
-                service,
-                errorcode,
-            )
+                headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {self._api_key}",
+                }
+                if method == "get":
+                    response = requests_get(
+                        f"{self._url}{service}",
+                        headers=headers,
+                        params=params,
+                        verify=self._ssl_verify,
+                        timeout=10,
+                    )
 
-            if (
-                errorcode != 500
-                and service != "reporting/get_data"
-                and service != "reporting/netdata_get_data"
-            ):
+                elif method == "post":
+                    response = requests_post(
+                        f"{self._url}{service}",
+                        headers=headers,
+                        json=params,
+                        verify=self._ssl_verify,
+                        timeout=10,
+                    )
+
+                if response.status_code == 200:
+                    data = response.json()
+                    _LOGGER.debug("TrueNAS %s query response: %s", self._host, data)
+                    self._error = ""
+                else:
+                    # if we didn't get a 200, we are still connected, but there was no valid data
+                    _LOGGER.warning(
+                        'TrueNAS %s error while fetch data "%s" (%s)',
+                        self._host,
+                        service,
+                        response.status_code,
+                    )
+                    self._error = response.status_code
+                    return None
+            except Exception as exception:
+                error = str(exception)
+                _LOGGER.warning(
+                    'TrueNAS %s unable to fetch data "%s" (%s)',
+                    self._host,
+                    service,
+                    error
+                )
                 self._connected = False
+                self._error = error
+                return None
 
-            self._error = errorcode
-            self.lock.release()
-            return None
-
-        self._connected = True
-        self._error = ""
-        self.lock.release()
-
-        return data
+            self._connected = True
+            return data
 
     @property
     def error(self):
