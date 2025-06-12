2924eb85912b1458ffb962637897263b9bd3d68c
Tomaae <23486452+tomaae@users.noreply.github.com>
2025-06-12 11:12:41 +0200

    Added reconfigure #210

--- custom_components/truenas/config_flow.py
+++ custom_components/truenas/config_flow.py
@@ -2,12 +2,17 @@
 
 from __future__ import annotations
 
+from collections.abc import Mapping
 from logging import getLogger
 from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.config_entries import CONN_CLASS_LOCAL_POLL, ConfigFlow
+from homeassistant.config_entries import (
+    CONN_CLASS_LOCAL_POLL,
+    ConfigFlow,
+    ConfigFlowResult,
+)
 from homeassistant.const import (
     CONF_API_KEY,
     CONF_HOST,
@@ -26,6 +31,41 @@ from .const import (
 from .api import TrueNASAPI
 
 _LOGGER = getLogger(__name__)
+
+
+def _base_schema(truenas_config: Mapping[str, Any]) -> vol.Schema:
+    """Generate base schema."""
+    base_schema = {
+        vol.Required(
+            CONF_NAME, default=truenas_config.get(CONF_NAME) or DEFAULT_DEVICE_NAME
+        ): str,
+        vol.Required(
+            CONF_HOST, default=truenas_config.get(CONF_HOST) or DEFAULT_HOST
+        ): str,
+        vol.Required(CONF_API_KEY, default=truenas_config.get(CONF_API_KEY) or ""): str,
+        vol.Required(
+            CONF_VERIFY_SSL,
+            default=truenas_config.get(CONF_VERIFY_SSL) or DEFAULT_SSL_VERIFY,
+        ): bool,
+    }
+
+    return vol.Schema(base_schema)
+
+
+def _reconfigure_schema(truenas_config: Mapping[str, Any]) -> vol.Schema:
+    """Generate base schema."""
+    base_schema = {
+        vol.Required(
+            CONF_HOST, default=truenas_config.get(CONF_HOST) or DEFAULT_HOST
+        ): str,
+        vol.Required(CONF_API_KEY, default=truenas_config.get(CONF_API_KEY) or ""): str,
+        vol.Required(
+            CONF_VERIFY_SSL,
+            default=truenas_config.get(CONF_VERIFY_SSL) or DEFAULT_SSL_VERIFY,
+        ): bool,
+    }
+
+    return vol.Schema(base_schema)
 
 
 # ---------------------------
@@ -48,33 +88,36 @@ class TrueNASConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = CONN_CLASS_LOCAL_POLL
 
-    async def async_step_import(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Occurs when a previous entry setup fails and is re-initiated."""
-        return await self.async_step_user(user_input)
+    def __init__(self) -> None:
+        """Initialize the config flow."""
+        self.truenas_config: dict[str, Any] = {}
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    ) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
+        truenas_config = self.truenas_config
         errors = {}
+
         if user_input is not None:
+            truenas_config.update(user_input)
+
             # Check if instance with this name already exists
-            if user_input[CONF_NAME] in configured_instances(self.hass):
+            if truenas_config[CONF_NAME] in configured_instances(self.hass):
                 errors["base"] = "name_exists"
 
             # Test connection
             api = await self.hass.async_add_executor_job(
                 TrueNASAPI,
-                user_input[CONF_HOST],
-                user_input[CONF_API_KEY],
-                user_input[CONF_VERIFY_SSL],
+                truenas_config[CONF_HOST],
+                truenas_config[CONF_API_KEY],
+                truenas_config[CONF_VERIFY_SSL],
             )
 
             conn, errorcode = await self.hass.async_add_executor_job(
                 api.connection_test
             )
+
             if not conn:
                 errors[CONF_HOST] = errorcode
                 _LOGGER.error("TrueNAS connection error (%s)", errorcode)
@@ -82,36 +125,51 @@ class TrueNASConfigFlow(ConfigFlow, domain=DOMAIN):
             # Save instance
             if not errors:
                 return self.async_create_entry(
-                    title=user_input[CONF_NAME], data=user_input
+                    title=truenas_config[CONF_NAME], data=truenas_config
                 )
 
-            return self._show_config_form(user_input=user_input, errors=errors)
-
-        return self._show_config_form(
-            user_input={
-                CONF_NAME: DEFAULT_DEVICE_NAME,
-                CONF_HOST: DEFAULT_HOST,
-                CONF_API_KEY: "",
-                CONF_VERIFY_SSL: DEFAULT_SSL_VERIFY,
-            },
+        return self.async_show_form(
+            step_id="user",
+            data_schema=_base_schema(truenas_config),
             errors=errors,
         )
 
-    def _show_config_form(
-        self, user_input: dict[str, Any] | None, errors: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Show the configuration form."""
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        truenas_config = self.truenas_config
+        reconfigure_entry = self._get_reconfigure_entry()
+        errors = {}
+
+        if user_input is not None:
+            truenas_config.update(user_input)
+
+            # Test connection
+            api = await self.hass.async_add_executor_job(
+                TrueNASAPI,
+                truenas_config[CONF_HOST],
+                truenas_config[CONF_API_KEY],
+                truenas_config[CONF_VERIFY_SSL],
+            )
+
+            conn, errorcode = await self.hass.async_add_executor_job(
+                api.connection_test
+            )
+
+            if not conn:
+                errors[CONF_HOST] = errorcode
+                _LOGGER.error("TrueNAS connection error (%s)", errorcode)
+
+            # Save instance
+            if not errors:
+                return self.async_update_reload_and_abort(
+                    self._get_reconfigure_entry(),
+                    title=reconfigure_entry.data[CONF_NAME],
+                    data_updates=truenas_config,
+                )
+
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_NAME, default=user_input[CONF_NAME]): str,
-                    vol.Required(CONF_HOST, default=user_input[CONF_HOST]): str,
-                    vol.Required(CONF_API_KEY, default=user_input[CONF_API_KEY]): str,
-                    vol.Optional(
-                        CONF_VERIFY_SSL, default=user_input[CONF_VERIFY_SSL]
-                    ): bool,
-                }
-            ),
+            step_id="reconfigure",
+            data_schema=_reconfigure_schema(reconfigure_entry.data),
             errors=errors,
         )
